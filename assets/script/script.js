a58ca007c146fa3971cf04b2cb9a1187af60aaa1
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-05-20 01:48:41 -0400

    fixing major issues with functions

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,6 +3,11 @@ var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l"
 var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericChars= ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChars = ["!", "$", "%", "&", "?", "@"];
+var passwordLength;
+var lowercase;
+var uppercase;
+var numerics;
+var specials;
 var pass;
 
 
@@ -10,109 +15,111 @@ var pass;
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
-}
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
 
 // Function to run a series of prompts for user. 
-var generatePassword = () => {
+
 
   // Prompt user for password length
-  var passwordLength = () => { 
-    prompt("How long does your password need to be? (8-128 characters)");
+  function lengthPrompt () { 
+    passwordLength = prompt("How long does your password need to be? (8-128 characters)");
     if (passwordLength < 8 && passwordLength > 128) {
       alert("Error: please enter a number between 8 and 128.");
       var passwordLength = prompt("How long does your password need to be? (8-128 characters)");
       passwordLength = parseInt(passwordLength, 10);
-      return passwordLength;
+      lengthPrompt();
       }
     else {
-      lowercase();
+      lowercasePrompt();
+      return passwordLength;
       }
   }
 
   // Prompt if password should contain lowercase letters
-  var lowercase = () => {
-    prompt("Should your password include lowercase letters? (please enter 'y' or 'n')");
+  function lowercasePrompt() {
+    lowercase = prompt("Should your password include lowercase letters? (please enter 'y' or 'n')");
     if (lowercase === 'y') {
       possibleChars = possibleChars.concat(lowercaseChars);
-      uppercase();
+      uppercasePrompt();
     }
     else if (lowercase === 'n') {
       alert("Error: please enter 'y' or 'n'.")
-      uppercase();
+      uppercasePrompt();
     }
     else {
       alert("Error: please enter 'y' or 'n'.");
-      lowercase();
+      lowercasePrompt();
     }
   }
 
   // prompt if password should contain uppercase letters
-  var uppercase = () => {
-    prompt("Should your password include uppercase letters? (please enter 'y' or 'n')");
+  function uppercasePrompt() {
+    uppercase = prompt("Should your password include uppercase letters? (please enter 'y' or 'n')");
     if (uppercase === 'y') {
       possibleChars = possibleChars.concat(uppercaseChars);
-      numerics();
+      numericsPrompt();
     }
     else if (uppercase === 'n') {
       alert("Error: please enter 'y' or 'n'.")
-      numerics();
+      numericsPrompt();
     }
     else {
       alert("Error: please enter 'y' or 'n'.");
-      uppercase();
+      uppercasePrompt();
     }
   }
 
   // prompt if password should contain numbers
-  var numerics = () => {
-    prompt("Should your password include numbers letters? (please enter 'y' or 'n')");
+  function numericsPrompt() {
+    numerics = prompt("Should your password include numbers letters? (please enter 'y' or 'n')");
     if (numerics === 'y') {
       possibleChars = possibleChars.concat(numericChars);
-      specials();
+      specialsPrompt();
     }
     else if (numerics === 'n') {
       alert("Error: please enter 'y' or 'n'.")
-      specials();
+      specialsPrompt();
     }
     else {
       alert("Error: please enter 'y' or 'n'.");
-      numerics();
+      numericsPrompt();
     }
   }
 
   // prompt if password should contain special characters
-  var specials = () => {
-    prompt("Should your password include lowercase letters? (please enter 'y' or 'n')");
+  function specialsPrompt() {
+   specials = prompt("Should your password include lowercase letters? (please enter 'y' or 'n')");
     if (specials === 'y') {
       possibleChars = possibleChars.concat(specialChars);
-      generatePassword();
+      newPassword();
     }
     else if (specials === 'n') {
       alert("Error: please enter 'y' or 'n'.")
-      generatePassword();
+      newPassword();
     }
     else {
       alert("Error: please enter 'y' or 'n'.");
-      specials();
+      specialsPrompt();
     }
   }
 
   // Generate password based on input from user in above prompts
-  var newPassword = () => {
+  function newPassword() {
     for (var i = 0; i < passwordLength; i++) {
       var nextChar = (Math.floor(Math.random() * possibleChars.length));
       pass += possibleChars[nextChar];
     }
-        return pass;
+    return pass;
   }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = pass;
 }
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", lengthPrompt);
