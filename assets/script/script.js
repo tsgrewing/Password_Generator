1b66a67618651a0cd7006840e5904f1814cff5d1
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-05-20 03:09:06 -0400

    cleaning up code

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,20 +4,10 @@ var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var numericChars= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChars = ["!", "#", "$", "%", "&,", "(", ")", "*", "+", "-", ".", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
 var passwordLength;
-var lowercase;
-var uppercase;
-var numerals;
-var specials;
-var pass;
 var password;
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-
-
-
-
 
 //run a series of prompts for user. 
 
@@ -116,11 +106,9 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   possibleChars = [];
-
   lengthPrompt();
   var password = newPassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 // Add event listener to generate button
