69a851c026945e6dfd0ced89020956c801230832
Tony G <63308979+tsgrewing@users.noreply.github.com>
2020-05-22 01:14:39 -0400

    Working on ensuring all required types of characters are included and evenly distributed

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,9 +2,15 @@ var possibleChars = [];
 var lowercaseChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericChars= ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChars = ["!", "#", "$", "%", "&,", "(", ")", "*", "+", "-", ".", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "~"];
+var specialChars = ["!", "#", "$", "%", "&,", "(", ")", "*", "+", "-", ".", "<", "=", ">", "?", "@", "[", "]", ";", "^", "_", "{", "}", "|", ":",  "~"];
 var passwordLength;
 var password;
+var lowercase;
+var uppercase;
+var numerals;
+var specials;
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 //run a series of prompts for user. 
@@ -22,71 +28,58 @@ function lengthPrompt () {
 }
 // Prompt if password should contain lowercase letters
 function lowercasePrompt() {
-  lowercase = prompt("Should your password include lowercase letters? (please enter 'y' or 'n')");
-  if (lowercase === "y") {
+  lowercase = confirm("Should your password include lowercase letters?");
+  if (lowercase) {
     possibleChars = possibleChars.concat(lowercaseChars);
     uppercasePrompt();
   }
-  else if (lowercase === "n") {
-    uppercasePrompt();
-  }
-  else {
-    alert("Error: please enter 'y' or 'n'.");
-    lowercasePrompt();
+  else if (!lowercase) {
+    uppercasePrompt();  
   }
 }
 // prompt if password should contain uppercase letters
 function uppercasePrompt() {
-  uppercase = prompt("Should your password include uppercase letters? (please enter 'y' or 'n')");
-  if (uppercase === "y") {
+  uppercase = confirm("Should your password include uppercase letters?");
+  if (uppercase) {
     possibleChars = possibleChars.concat(uppercaseChars);
     numeralsPrompt();
   }
-  else if (uppercase === "n") {
+  else if (!uppercase) {
     numeralsPrompt();
-  }
-  else {
-    alert("Error: please enter 'y' or 'n'.");
-    uppercasePrompt();
   }
 }
 // prompt if password should contain numbers
 function numeralsPrompt() {
-  numerals = prompt("Should your password include numbers? (please enter 'y' or 'n')");
-  if (numerals === "y") {
+  numerals = confirm("Should your password include numbers?");
+  if (numerals) {
     possibleChars = possibleChars.concat(numericChars);
     specialsPrompt();
   }
-  else if (numerals === "n") {
+  else if (!numerals) {
     specialsPrompt();
-  }
-  else {
-    alert("Error: please enter 'y' or 'n'.");
-    numeralsPrompt();
   }
 }
 // prompt if password should contain special characters
 function specialsPrompt() {
-  specials = prompt("Should your password include special characters? (please enter 'y' or 'n')");
-  if (specials === "y") {
+  specials = confirm("Should your password include special characters?");
+  if (specials) {
     possibleChars = possibleChars.concat(specialChars);
   }
-  else if (specials === "n" && numerals === "n" && lowercase === "n" && uppercase ==="n") {
+  else if (!specials && !numerals && !lowercase && !uppercase) {
     alert("Error: Please select at least one type of character to include in your password.")
     lowercasePrompt();
-  }
-  else if (specials !== 'n') {
-    alert("Error: please enter 'y' or 'n'.");
-    specialsPrompt();
   }
 }
 // Generate password based on input from user in above prompts
 function generatePassword() {
   password = "";
-  for (var i = 0; i < passwordLength ; i++) {
-    var nextChar = (Math.floor(Math.random() * possibleChars.length));
-    password += possibleChars[nextChar];
-  }
+
+
+    // if(var2 < passwordLength) {}  
+      for (var i = 0; i < (passwordLength ) ; i++) {
+        var nextChar = (Math.floor(Math.random() * possibleChars.length));
+        password += possibleChars[nextChar];
+      }
   return password;
 }
 // Write password to the #password input
